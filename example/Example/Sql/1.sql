4de01bb88e7b084b560cba49a82743e234b075ea
SonarBeserk <sonarbeserk@gmail.com>
2025-07-01 00:21:21 -0400

    Add table for stats

--- example/Example/Sql/1.sql
+++ example/Example/Sql/1.sql
@@ -4,3 +4,10 @@
     name     TEXT             NOT NULL,
     nickname TEXT             NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS stats
+(
+    stat_id TEXT PRIMARY KEY NOT NULL,
+    name    TEXT             NOT NULL,
+    sent_at TEXT             NOT NULL
+)
